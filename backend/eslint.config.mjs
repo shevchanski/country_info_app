a404b699606bed60d1d9c570bcd6cb4abb023700
Oleksii Shevchenko <rembo.51423@gmail.com>
2024-10-28 14:35:19 +0200

    chore(BE/eslint): add rule 'no-console' & add ingores build-dir

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,9 +3,16 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["./build/**.{js,mjs,cjs,ts}"],
+  },
+  {
+    languageOptions: { globals: globals.node },
+    rules: {
+      "no-console": ["error", { allow: ["info", "error"] }],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 );
-
