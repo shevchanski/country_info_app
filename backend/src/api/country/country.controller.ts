0275af2624408a0c46f358a21c24755711127dd9
Oleksii Shevchenko <rembo.51423@gmail.com>
2024-10-28 14:28:55 +0200

    feat(BE/api/country/controller): add try-catch with next(err)

--- backend/src/api/country/country.controller.ts
+++ backend/src/api/country/country.controller.ts
@@ -1,12 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import RES_STATUS from "../../config/status.config";
 import CountryService from "./country.service";
 
 class CountryController {
-  static async getAllCountries(req: Request, res: Response) {
-    const countries = await CountryService.getAllCountries();
+  static async getAllCountries(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const countries = await CountryService.getAllCountries();
 
-    res.status(RES_STATUS.OK).json({ countries: countries });
+      res.status(RES_STATUS.OK).json({ countries: countries });
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
