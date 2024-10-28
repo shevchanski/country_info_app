f23d704ce2b00d3149e30f7cb468b1486993a4c6
Oleksii Shevchenko <rembo.51423@gmail.com>
2024-10-28 14:27:48 +0200

    feat(BE/country/service): create getCountryInfo-method

--- backend/src/api/country/country.service.ts
+++ backend/src/api/country/country.service.ts
@@ -1,19 +1,62 @@
 import axios from "axios";
-import { IAvailableCountry } from "../../types/types";
+import {
+  CountriesNowResponse,
+  FlagResponse,
+  IAvailableCountry,
+  ICountryInfo,
+  NagerCountryInfo,
+  PopulationResponse,
+} from "../../types/types";
 
 class CountryService {
-  static readonly API_URL?: string = process.env.NAGER_API_URL;
+  static readonly NAGER_URL?: string = process.env.NAGER_API_URL;
+  static readonly POPUL_URL: string =
+    "https://countriesnow.space/api/v0.1/countries/";
 
   static async getAllCountries(): Promise<IAvailableCountry[]> {
-    if (!this.API_URL) {
+    if (!this.NAGER_URL) {
       throw new Error("API_URL is undefined");
     }
 
     const response = await axios.get<IAvailableCountry[]>(
-      `${this.API_URL}/AvailableCountries`,
+      `${this.NAGER_URL}/AvailableCountries`,
     );
 
     return response.data;
+  }
+
+  static async getCountryInfo(countryCode: string): Promise<ICountryInfo> {
+    if (!this.NAGER_URL) {
+      throw new Error("API_URL is undefined");
+    }
+
+    const { data: countryData } = await axios.get<NagerCountryInfo>(
+      `${this.NAGER_URL}/CountryInfo/${countryCode}`,
+    );
+
+    const populDataPromise = axios.post<
+      CountriesNowResponse<PopulationResponse>
+    >(`${this.POPUL_URL}/population`, {
+      country: countryData.commonName,
+    });
+
+    const countryFlagPromise = axios.post<CountriesNowResponse<FlagResponse>>(
+      `${this.POPUL_URL}/flag/images`,
+      {
+        country: countryData.commonName,
+      },
+    );
+
+    const [populData, flagData] = await Promise.all([
+      populDataPromise,
+      countryFlagPromise,
+    ]);
+
+    return {
+      list_of_borders: countryData.borders,
+      population_data: populData.data.data.populationCounts,
+      flag_url: flagData.data.data.flag,
+    };
   }
 }
 
--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -2,3 +2,42 @@ export interface IAvailableCountry {
   countryCode: string;
   name: string;
 }
+
+export interface NagerCountryInfo {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: string[];
+}
+
+interface PopulationData {
+  year: number;
+  value: number;
+}
+
+export interface CountriesNowResponse<T> {
+  error: boolean;
+  msg: string;
+  data: T;
+}
+
+export interface PopulationResponse {
+  country: string;
+  code: string;
+  iso3: string;
+  populationCounts: PopulationData[];
+}
+
+export interface FlagResponse {
+  name: string;
+  flag: string;
+  iso2: string;
+  iso3: string;
+}
+
+export interface ICountryInfo {
+  list_of_borders: string[];
+  population_data: PopulationData[];
+  flag_url: string;
+}
